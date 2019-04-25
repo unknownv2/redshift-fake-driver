69b30704dbb7cd650ed754bc2b41be00082004cf
Thierry <thierry44@gmail.com>
2019-04-25 00:33:51 -0400

    Change default AWS S3 region to us-east-1

--- src/main/scala/jp/ne/opt/redshiftfake/Global.scala
+++ src/main/scala/jp/ne/opt/redshiftfake/Global.scala
@@ -8,5 +8,5 @@ import com.amazonaws.regions.{Regions, Region, RegionUtils}
 object Global {
   def s3Endpoint: String = sys.props.getOrElse("fake.awsS3Endpoint", "s3://")
   def s3Scheme: String = sys.props.getOrElse("fake.awsS3Scheme", s3Endpoint)
-  def region: Region = RegionUtils.getRegion(sys.props.getOrElse("fake.awsRegion", Regions.AP_NORTHEAST_1.getName))
+  def region: Region = RegionUtils.getRegion(sys.props.getOrElse("fake.awsRegion", Regions.US_EAST_1.getName))
 }
